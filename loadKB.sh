27870c0e4764f00410b245b0877d218672c9405b
Rob Court <robbie.court@gmail.com>
2017-04-28 15:33:09 +0100

    clearing backup away

--- loadKB.sh
+++ loadKB.sh
@@ -5,6 +5,8 @@ curl -O http://data.virtualflybrain.org/archive/VFB-KB.tar.gz
 tar -xzvf /opt/VFB/backup/VFB-KB.tar.gz
 chmod -R 777 /opt/VFB
 /var/lib/neo4j/bin/neo4j-admin restore --from=/opt/VFB/backup/VFB-KB.db --force=true
+rm -rf /opt/VFB/backup
+
 
 sed -i s/read_only=false/read_only=${NEOREADONLY}/ ${NEOSERCONF} 
 
