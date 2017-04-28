aed0130c779f4e1a35536fd0abba948cd51edd07
Rob Court <robbie.court@gmail.com>
2017-04-28 17:52:39 +0100

    ensuring everything runs

--- loadKB.sh
+++ loadKB.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-#Resore KB from current backup
-cd /opt/VFB/backup/
-curl -O http://data.virtualflybrain.org/archive/VFB-KB.tar.gz 
-tar -xzvf /opt/VFB/backup/VFB-KB.tar.gz
-chmod -R 777 /opt/VFB
-/var/lib/neo4j/bin/neo4j-admin restore --from=/opt/VFB/backup/VFB-KB.db --force=true
-cd /var/lib/neo4j
-rm -rf /opt/VFB/backup/*
-
-# set read_only 
-sed -i s/read_only=false/read_only=${NEOREADONLY}/ ${NEOSERCONF} 
-
-# start Neo4j service
+#Resore KB from current backup, set read only then launch neo4j service
+cd /opt/VFB/backup/ && \
+curl -O http://data.virtualflybrain.org/archive/VFB-KB.tar.gz  && \
+tar -xzvf /opt/VFB/backup/VFB-KB.tar.gz && \
+chmod -R 777 /opt/VFB && \
+/var/lib/neo4j/bin/neo4j-admin restore --from=/opt/VFB/backup/VFB-KB.db --force=true && \
+cd /var/lib/neo4j && \
+rm -rf /opt/VFB/backup/* && \
+sed -i s/read_only=false/read_only=${NEOREADONLY}/ ${NEOSERCONF} && \
 exec /docker-entrypoint.sh neo4j
