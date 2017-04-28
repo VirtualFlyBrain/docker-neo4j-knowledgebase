b4f3a7f85312fdc294ee8765b54057e664acdce7
Rob Court <robbie.court@gmail.com>
2017-04-28 17:49:23 +0100

    Update loadKB.sh

--- loadKB.sh
+++ loadKB.sh
@@ -7,7 +7,7 @@ tar -xzvf /opt/VFB/backup/VFB-KB.tar.gz
 chmod -R 777 /opt/VFB
 /var/lib/neo4j/bin/neo4j-admin restore --from=/opt/VFB/backup/VFB-KB.db --force=true
 cd /var/lib/neo4j
-rm -rf /opt/VFB/backup
+rm -rf /opt/VFB/backup/*
 
 # set read_only 
 sed -i s/read_only=false/read_only=${NEOREADONLY}/ ${NEOSERCONF} 
