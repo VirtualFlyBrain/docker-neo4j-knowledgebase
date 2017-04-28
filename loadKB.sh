5e8bfae3e63d865c3054113da29b8f1765ab69ab
Rob Court <robbie.court@gmail.com>
2017-04-28 15:43:04 +0100

    Update loadKB.sh

--- loadKB.sh
+++ loadKB.sh
@@ -5,6 +5,7 @@ curl -O http://data.virtualflybrain.org/archive/VFB-KB.tar.gz
 tar -xzvf /opt/VFB/backup/VFB-KB.tar.gz
 chmod -R 777 /opt/VFB
 /var/lib/neo4j/bin/neo4j-admin restore --from=/opt/VFB/backup/VFB-KB.db --force=true
+cd /
 rm -rf /opt/VFB/backup
 
 
