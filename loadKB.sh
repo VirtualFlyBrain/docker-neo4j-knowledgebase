0a4218501b3880f5055237732993f6b9e22e0d09
Rob Court <robbie.court@gmail.com>
2017-04-29 11:09:32 +0100

    Update loadKB.sh

--- loadKB.sh
+++ loadKB.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 #Resore KB from current backup, set read only then launch neo4j service
-cd /opt/VFB/backup/ && \
+cd /opt/VFB/backup/
 curl --retry 3 --retry-delay 5 -O http://data.virtualflybrain.org/archive/VFB-KB.tar.gz  
-tar -xzvf /opt/VFB/backup/VFB-KB.tar.gz && \
+tar -xzvf /opt/VFB/backup/VFB-KB.tar.gz 
 chmod -R 777 /opt/VFB && \
 /var/lib/neo4j/bin/neo4j-admin restore --from=/opt/VFB/backup/VFB-KB.db --force=true && \
 cd /var/lib/neo4j && \
