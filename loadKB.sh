9d59fcf3d8418e3ef5f0898dc5f98827a5ad6c86
Rob Court <robbie.court@gmail.com>
2017-06-15 16:56:32 +0100

    logic fix

--- loadKB.sh
+++ loadKB.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -d /backup/KBW-RESTORE.db ]; then
+if [ -d /backup/KBW-RESTORE.db ]; then
   echo 'Resore KB from given backup'
   /var/lib/neo4j/bin/neo4j-admin restore --from=/backup/KBW-RESTORE.db --force=true && \
   cd /var/lib/neo4j && \
