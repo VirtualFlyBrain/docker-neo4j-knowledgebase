e60ac5c8c72bfb1dc26d409ebce9918f4f510bc6
Rob Court <robbie.court@gmail.com>
2017-06-15 10:11:58 +0100

    syntax fix

--- loadKB.sh
+++ loadKB.sh
@@ -8,7 +8,7 @@ if [ ! -d /data/databases/graph.db ]; then
   chmod -R 777 /opt/VFB && \
   /var/lib/neo4j/bin/neo4j-admin restore --from=/opt/VFB/backup/VFB-KB.db --force=true && \
   cd /var/lib/neo4j && \
-  rm -rf /opt/VFB/backup/* && \
+  rm -rf /opt/VFB/backup/*;
 fi
 sed -i s/read_only=false/read_only=${NEOREADONLY}/ ${NEOSERCONF} && \
 exec /docker-entrypoint.sh neo4j
