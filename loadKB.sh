2deb2d77ddbb3d76557d6584d9b11e9f58d7ea22
Rob Court <robbie.court@gmail.com>
2017-06-15 10:22:22 +0100

    opening folder name

--- loadKB.sh
+++ loadKB.sh
@@ -6,7 +6,7 @@ if [ ! -d /data/databases/graph.db ]; then
   wget -c http://data.virtualflybrain.org/archive/VFB-KB.tar.gz  
   tar -xzvf /opt/VFB/backup/VFB-KB.tar.gz 
   chmod -R 777 /opt/VFB && \
-  /var/lib/neo4j/bin/neo4j-admin restore --from=/opt/VFB/backup/VFB-KB.db --force=true && \
+  /var/lib/neo4j/bin/neo4j-admin restore --from=/opt/VFB/backup/*.db* --force=true && \
   cd /var/lib/neo4j && \
   rm -rf /opt/VFB/backup/*;
 fi
