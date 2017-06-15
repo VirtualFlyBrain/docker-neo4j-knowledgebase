2beb472327788d6415d80f9b8fe07370afe18384
Rob Court <robbie.court@gmail.com>
2017-06-15 15:37:33 +0100

    using local KBW-RESTORE.db folder for restoration

--- loadKB.sh
+++ loadKB.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-if [ ! -d /data/databases/graph.db ]; then
-  #Resore KB from current backup, set read only then launch neo4j service
-  cd /opt/VFB/backup/
-  wget -c http://data.virtualflybrain.org/archive/VFB-KB.tar.gz  
-  tar -xzvf /opt/VFB/backup/VFB-KB.tar.gz 
-  chmod -R 777 /opt/VFB && \
-  /var/lib/neo4j/bin/neo4j-admin restore --from=/opt/VFB/backup/*.db* --force=true && \
+if [ ! -d /backup/KBW-RESTORE.db ]; then
+  echo 'Resore KB from given backup'
+  /var/lib/neo4j/bin/neo4j-admin restore --from=/backup/KBW-RESTORE.db --force=true && \
   cd /var/lib/neo4j && \
-  rm -rf /opt/VFB/backup/*;
+  rm -rf /backup/KBW-RESTORE.db;
 fi
+echo "set read only = ${NEOREADONLY} then launch neo4j service"
 sed -i s/read_only=false/read_only=${NEOREADONLY}/ ${NEOSERCONF} && \
 exec /docker-entrypoint.sh neo4j
