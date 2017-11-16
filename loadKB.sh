0af8477c5e83d8e94154d302f847c7f0f19eeafb
Rob Court <robbie.court@gmail.com>
2017-11-16 13:28:04 +0000

    no longer remove backup

--- loadKB.sh
+++ loadKB.sh
@@ -13,9 +13,7 @@ fi
 
 if [ -d /backup/KBW-RESTORE.db ]; then
   echo 'Resore KB from given backup'
-  /var/lib/neo4j/bin/neo4j-admin restore --from=/backup/KBW-RESTORE.db --force=true && \
-  cd /var/lib/neo4j && \
-  rm -rf /backup/KBW-RESTORE.db;
+  /var/lib/neo4j/bin/neo4j-admin restore --from=/backup/KBW-RESTORE.db --force=true
 fi
 echo "set read only = ${NEOREADONLY} then launch neo4j service"
 sed -i s/read_only=false/read_only=${NEOREADONLY}/ ${NEOSERCONF} && \
