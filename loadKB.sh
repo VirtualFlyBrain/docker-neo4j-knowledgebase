65436f54093e8d3a8b5906255c4281caa38c98e9
Rob Court <robbie.court@gmail.com>
2018-01-18 20:19:01 +0000

    location fix

--- loadKB.sh
+++ loadKB.sh
@@ -7,7 +7,7 @@ if [ ! -d /data/databases/graph.db ]; then
     rm /opt/VFB/backup/VFB-KB.tar.gz
     wget http://data.virtualflybrain.org/archive/VFB-KB.tar.gz 
     tar -xzvf VFB-KB.tar.gz
-    find /opt/VFB/backup/ -name 'KBW-RESTORE.db' -exec cp -vr "{}" /backup/ +
+    find /opt/VFB/backup/ -name 'KBW-RESTORE.db' -exec cp -vr "{}" /backup/KBW-RESTORE.db/ +
     rm -rf /opt/VFB/backup/*
   fi
 fi
