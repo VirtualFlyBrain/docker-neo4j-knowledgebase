23c578c4efc9b7a039746e387fe8cd4f7f30809d
Rob Court <robbie.court@gmail.com>
2018-02-02 10:17:21 +0000

    adding query logging

--- loadKB.sh
+++ loadKB.sh
@@ -20,4 +20,10 @@ if [ -d /backup/KBW-RESTORE.db ]; then
 fi
 echo "set read only = ${NEOREADONLY} then launch neo4j service"
 sed -i s/read_only=.*/read_only=${NEOREADONLY}/ ${NEOSERCONF} && \
+
+echo -e '\nSTARTING VFB KB SERVER\n' >> /var/lib/neo4j/logs/query.log
+
+#Output the query log to docker log:
+tail -f /var/lib/neo4j/logs/query.log >/proc/1/fd/1 &
+
 exec /docker-entrypoint.sh neo4j
