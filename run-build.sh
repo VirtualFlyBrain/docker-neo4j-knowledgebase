5f0fe5245cd1e5d19362a5ec6171d42f333b4167
matentzn <nicolas.matentzoglu@gmail.com>
2019-08-17 07:42:55 +0100

    New plugin version

---
+++ run-build.sh
@@ -0,0 +1,2 @@
+docker build -t virtualflybrain/docker-neo4j-knowledgebase:neo2owl .
+docker run -p:7474:7474 -p 7687:7687 --env=NEO4J_AUTH=neo4j/neo --env=NEO4J_dbms_read__only=false --env=NEO4J_dbms_memory_heap_maxSize=6G --env=NEO4J_dbms_memory_heap_initial__size=1G virtualflybrain/docker-neo4j-knowledgebase:neo2owl
