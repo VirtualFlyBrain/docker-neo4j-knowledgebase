28a94b1f105f0cf6e721c24dd4b5414b19f73d35
Rob Court <rcourt@ed.ac.uk>
2019-10-01 11:01:10 +0100

    clearing old users out

--- loadKB.sh
+++ loadKB.sh
@@ -26,4 +26,7 @@ echo -e '\nSTARTING VFB KB SERVER\n' >> /var/lib/neo4j/logs/query.log
 #Output the query log to docker log:
 tail -f /var/lib/neo4j/logs/query.log >/proc/1/fd/1 &
 
+#clear auth from backup
+rm -rf /data/dbms/auth
+
 exec /docker-entrypoint.sh neo4j
