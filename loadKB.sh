7c9bea6dccbe9b44e34ad778b0d5339da4f0a15a
Rob Court <rcourt@ed.ac.uk>
2021-03-31 16:25:41 +0100

    removing backup restore

--- loadKB.sh
+++ loadKB.sh
@@ -4,31 +4,6 @@
 echo "Sourcing remote config"
 source ${CONF_DIR}/config.env
 
-if [ ! -d /data/databases/graph.db ]; then
-  if [ ! -d /backup/KBW-RESTORE.db ]; then
-    echo 'Resore KB from archive backup'
-    cd /opt/VFB/backup/
-    rm /opt/VFB/backup/VFB-KB-4-2.tar.gz
-    wget ${KB_DATA}
-    tar -xzvf *.tar.gz
-    rm -rf /var/lib/neo4j/data/*
-    cp -r databases /var/lib/neo4j/data/
-    cp -r dbms /var/lib/neo4j/data/
-
-    rm -rf /opt/VFB/backup/*
-    cd -
-  fi
-  # if [ -d /backup/KBW-RESTORE.db ]; then
-  #   echo 'Resore KB from given backup'
-  #   # /var/lib/neo4j/bin/neo4j-admin restore --from=/backup/KBW-RESTORE.db --database=neo4j --force
-  #   rm -r /var/lib/neo4j/data/.
-  #   cp -r /backup/KBW-RESTORE.db/databases /var/lib/neo4j/data/
-  #   cp -r /backup/KBW-RESTORE.db/dbms /var/lib/neo4j/data/
-  #   # /var/lib/neo4j/bin/neo4j-admin restore --from=/backup/KBW-RESTORE.db --force=true
-  #   echo 'Restore complete'
-  # fi
-fi
-
 echo "set read only = ${NEOREADONLY} then launch neo4j service"
 sed -i s/read_only=.*/read_only=${NEOREADONLY}/ ${NEOSERCONF} && \
 
